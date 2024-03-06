8d8a7ae3983ccbde83be1cb82269cd8492f3b84c
sangaryousmane <sangaryousmane@gmail.com>
2024-03-06 19:35:03 +0000

    Final User

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
- const allPromises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
- 
- const results = await Promise.allSettled(allPromises);
- 
- return results.map(result => ({
- status: result.status,
- value: result.status === 'fulfilled' ? result.value : result.reason
- })
+  const allPromises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+
+  const results = await Promise.allSettled(allPromises);
+
+  return results.then((res) => res.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
+  })));
 }
