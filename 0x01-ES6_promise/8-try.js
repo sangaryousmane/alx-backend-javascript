e41f4c3f00442bb0448e85b0adfd254a6e8f9008
sangaryousmane <sangaryousmane@gmail.com>
2024-03-06 17:35:35 +0000

    Throw error / try catch

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+export default function divideFunction(numerator, denominator) {
+  try {
+    return `${numerator / denominator}`;
+  } catch (err) {
+    throw new Error('cannot divide by 0');
+  }
+}
