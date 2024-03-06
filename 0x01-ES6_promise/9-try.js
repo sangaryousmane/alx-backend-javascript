725a2d4e0705ba489c07b834058674d4f7de878a
sangaryousmane <sangaryousmane@gmail.com>
2024-03-06 17:47:18 +0000

    Throw an error and handle default all tlt each time

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+const guardrail = (mathFunction) => {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+};
+export default guardrail;
