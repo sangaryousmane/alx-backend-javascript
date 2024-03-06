79c35dd7b8b6136166076c089f87d50c3b58ce79
sangaryousmane <sangaryousmane@gmail.com>
2024-03-06 18:57:09 +0000

    When you promise, you have tto Await / Async

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    const mathResult = mathFunction();	  
+    const mathResult = mathFunction();
     queue.push(mathResult);
   } catch (err) {
-    queue.push(String(err.message));
+    queue.push(String(err));
   } finally {
     queue.push('Guardrail was processed');
   }
   return queue;
-};
+}
