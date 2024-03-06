389e36aa30d0e50406d45f37815669a716c2ad3e
sangaryousmane <sangaryousmane@gmail.com>
2024-03-06 19:05:54 +0000

    Reject the promises when uploading the a photo

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    return `${numerator / denominator}`;
-  } catch (err) {
-    throw new Error('cannot divide by 0');
-  }
+  if (denominator === 0)
+	throw new Error('cannot divide by 0');
+  else
+        return numerator / denominator;
 }
