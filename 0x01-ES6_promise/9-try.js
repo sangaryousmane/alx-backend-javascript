7a2bbc2952677a1a4ec24f4da4208485e0b862ac
sangaryousmane <sangaryousmane@gmail.com>
2024-03-06 17:58:59 +0000

    Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
-const guardrail = (mathFunction) => {
+export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    queue.push(mathFunction());
+    const mathResult = mathFunction();	  
+    queue.push(mathResult);
   } catch (err) {
-    queue.push(String(err));
+    queue.push(String(err.message));
   } finally {
     queue.push('Guardrail was processed');
   }
   return queue;
 };
-export default guardrail;
