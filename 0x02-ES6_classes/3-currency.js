f8d04fb4a0ba8640bcf55f6049da3fe6cf647400
sangaryousmane <sangaryousmane@gmail.com>
2024-03-08 19:01:11 +0000

    Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name}(${this._code})`;
+  }
+}
