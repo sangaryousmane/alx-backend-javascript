e3cc632f3e68705cf4f7e29a94abc79f5b30307e
sangaryousmane <sangaryousmane@gmail.com>
2024-03-08 18:09:33 +0000

    For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let value of array) {
-	  value = appendString + value;
-	  array.push(value);
+    array.push(vappendString + value);
   }
   return array;
 }
