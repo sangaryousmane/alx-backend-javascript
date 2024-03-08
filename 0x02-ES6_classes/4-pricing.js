f99523a7a90c2cc8693898a761fa80065b4b691d
sangaryousmane <sangaryousmane@gmail.com>
2024-03-08 19:37:27 +0000

    Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,34 +1,38 @@
-import Currency from "./3-currency";
-
+import Currency from './3-currency';
 
 export default class Pricing {
- constructor(amount, currency) {
- this.amount = amount;
- this.currency = currency;
- }
- 
- set amount(amount) {
- this._amount = amount;
- }
- 
- set currency(currency) {
- this._currency = currency;
- }
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
- get currency() {
- return this._currency;
- }	
+  set amount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    }
+  }
 
- get amount() {
- return this._amount;
- }	
+  set currency(currency) {
+    if (typeof currency === 'number') {
+       this._currency = currency;
+    }
+  }
 
- displayFullPrice() {
- return `${this._amount} ${this._currency.name} (${this._currency.code})`;
- }	
+  get currency() {
+    return this._currency;
+  }
 
- convertPrice(amount, conversionRate) {
- return amount * conversionRate;
- }
+  get amount() {
+    return this._amount;
+  }
 
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+   if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+   }
+  }
 }
