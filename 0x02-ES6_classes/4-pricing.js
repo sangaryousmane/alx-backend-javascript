2919ddeed47ad956c3a9292a53bd00889f01f94f
sangaryousmane <sangaryousmane@gmail.com>
2024-03-08 19:16:42 +0000

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from "./3-currency";
+
+
+export default class Pricing {
+ constructor(amount, currency) {
+ this.amount = amount;
+ this.currency = currency;
+ }
+ 
+ set amount(amount) {
+ this._amount = amount;
+ }
+ 
+ set currency(currency) {
+ this._currency = currency;
+ }
+
+ get currency() {
+ return this._currency;
+ }	
+
+ get amount() {
+ return this._amount;
+ }	
+
+ displayFullPrice() {
+ return `${this._amount} ${this._currency.name}  (${this._currency.code})`;
+ }	
+
+ convertPrice(amount, conversionRate) {
+ return `${amount * conversionRate}`;
+ }
+
+}
