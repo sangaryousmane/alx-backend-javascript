fc9a61fe5a92b91577fae377b3b9da57be4399df
sangaryousmane <sangaryousmane@gmail.com>
2024-03-08 02:35:34 +0000

    Implement a car: Vroom!

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,7 +5,35 @@ export default class Car {
     this._color = color;
   }
 
+  set brand(brand) {
+    this._brand = brand;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set motor(motor) {
+    this._motor = motor;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set color(color) {
+    this._color = color;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new this.constructor[Symbol.species]();
   }
 }
