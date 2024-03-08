5ddb621391bba0c55967d0a2aa84b0e281ef9c13
sangaryousmane <sangaryousmane@gmail.com>
2024-03-08 19:47:48 +0000

    Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -13,8 +13,8 @@ export default class Pricing {
   }
 
   set currency(currency) {
-    if (typeof currency === 'number') {
-       this._currency = currency;
+    if (currency instanceof Currency) {
+      this._currency = currency;
     }
   }
 
@@ -31,8 +31,10 @@ export default class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
-   if (typeof amount === 'number' && typeof conversionRate === 'number') {
-      return amount * conversionRate;
-   }
+    if ((typeof amount === 'number') && (typeof conversionRate === 'number')) {
+      const convertion = amount * conversionRate;
+      return convertion;
+    }
+    throw new TypeError('amount and conversion rate must be a number');
   }
 }
