6b221e391939770fc4e6fb3df191d6e444b600a9
sangaryousmane <sangaryousmane@gmail.com>
2024-03-08 18:37:14 +0000

    Iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,8 @@
+export default function iterateThroughObject(reportWithIterator) {
+ const arr = [];
+
+ for (const employee of reportWithIterator) {
+  arr.append(employee);
+ }
+ return arr.join(" | ");
+}
