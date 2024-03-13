0bb13475ceb4121597d18f9aedee937daae9c691
sangaryousmane <sangaryousmane@gmail.com>
2024-03-13 17:20:47 +0000

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,66 +1,77 @@
 
 interface DirectorInterface {
- workFromHome(): string;
- getCoffeeBreak(): string;
- workDirectorTasks(): string;
-}
-
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+   }
+   
 interface TeacherInterface {
- workFromHome(): string;
- getCoffeeBreak(): string;
- workTeacherTasks(): string;
+workFromHome(): string;
+getCoffeeBreak(): string;
+workTeacherTasks(): string;
 }
-
+   
 class Director implements DirectorInterface {
- 
+    
  workFromHome(): string {
- return "Working from home";
- }
-
- getToWork(): string {
- return "Getting a coffee break";
- }
-
- workDirectorTasks(): string {
- return "Getting to director tasks";
- }
-
+  return "Working from home";
+}
+   
+getCoffeeBreak(): string {
+return "Getting a coffee break";
+}
+   
+   
+workDirectorTasks(): string {
+return "Getting to director tasks";
 }
 
+}
+   
 class Teacher implements TeacherInterface {
- workFromHome(): string {
- return "Cannot work from home";
- }
-
- getCoffeeBreak(): string {
- return "Cannot have a break";
- }
-
- workTeacherTasks(): string {
- return "Getting to work";
- }
+workFromHome(): string {
+return "Cannot work from home";
 }
-
-const createEmployee: Teacher | Director = (salary: number | string) => {
- 
- if ((typeof salary === "number") && (salary < 500)) {
-    return new Teacher();
+   
+getCoffeeBreak(): string {
+return "Cannot have a break";
+}
+   
+workTeacherTasks(): string {
+return "Getting to work";
+}
+}
+   
+const createEmployee = (salary: number | string): Teacher | Director => {
+    
+  if ((typeof salary === "number") && (salary < 500)) {
+      return new Teacher();
    } else {
-    return new Director();
+      return new Director();
+   }
+}
+   
+  const isDirector = (employee: Teacher | Director) => {
+   return (employee as Director).workDirectorTasks !== undefined;
+}
+   
+ const executeWork = (employee: Teacher | Director) => {
+   if (isDirector(employee)) {
+      return employee.workDirectorTasks();
+   }
+   else {
+     return employee.workTeacherTasks();
    }
 }
 
-const isDirector = (employee?: Director) => {
- return typeof employee === Director;
-}
+type Subjects = "Math" | "History";
 
-const executeWork = (employee) => {
-  if (isDirector(employee)) {
-    employee.workDirectorTasks();
+const teachClass = (todayClass: Subjects): string => {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else if (todayClass === "History") {
+    return "Teaching History";
+  } else {
+    return "Invalid subject";
   }
-  else {
-   employee.workTeacherTasks();
-  }
-}
-
-
+};   
