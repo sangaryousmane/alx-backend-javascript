0a83d82c59450cbd29c172a90a88d8a3f2e04b83
sangaryousmane <sangaryousmane@gmail.com>
2024-06-15 03:09:55 +0000

    Add a console file as initial

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Logs a message to the console.
+ * and exports the displayMessage function
+ */
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Exports the displayMessage function.
+ */
+module.exports = displayMessage;
